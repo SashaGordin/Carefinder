38d04bf3f7272579430db9e9f174324fb26d5e83
SashaGordin <55962431+SashaGordin@users.noreply.github.com>
2024-01-29 20:19:30 -0800

    add connection/ storing data to firebase firestore database, add navbar to dashboard

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
-import { Card, Button, Alert } from 'react-bootstrap'
+import { Card, Button, Alert, Navbar, Image } from 'react-bootstrap'
 import { useAuth } from '../contexts/AuthContext'
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
-
+import logo from '../assets/Group.png';
+// import background from '../assets/Rectangle_7.png';
 
 export default function Dashboard() {
   const [error, setError] = useState('')
@@ -24,6 +25,20 @@ export default function Dashboard() {
 
   return (
     <>
+      <Navbar bg="dark" fixed="top">
+        {/* <div  style={{
+            backgroundImage: `url(${background})`,
+            backgroundSize: 'cover',
+            width: '50px',  // Adjust the width to match your icon
+            marginRight: '10px',
+            overflow: 'hidden',
+          }}> */}
+          <Image src={logo} alt="Logo Icon" style={{ width: '50px', marginRight: '15px', marginLeft: '5px' }} />
+        {/* </div> */}
+        <Navbar.Brand style={{ color: 'white'}}>
+          Carefinder
+        </Navbar.Brand>
+      </Navbar>
       <Card>
         <Card.Body>
           <h2 className="text-center mb-4">Dashboard</h2>
@@ -35,6 +50,7 @@ export default function Dashboard() {
         <Button variant="link" onClick={handleLogout}>
           Log Out
         </Button>
+        <Link to="/survey">Take Survey</Link>
       </div>
     </>
   )

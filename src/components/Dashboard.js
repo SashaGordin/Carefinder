120b968c4508645efc6b4e354a4254be4cf132d1
SashaGordin <55962431+SashaGordin@users.noreply.github.com>
2024-01-29 17:04:37 -0800

    add authentication with firebase

---
+++ src/components/Dashboard.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react'
+import { Card, Button, Alert } from 'react-bootstrap'
+import { useAuth } from '../contexts/AuthContext'
+import { useNavigate } from 'react-router-dom';
+
+
+
+export default function Dashboard() {
+  const [error, setError] = useState('')
+  const { logout } = useAuth();
+  const navigate = useNavigate();
+
+
+  async function handleLogout() {
+    setError('')
+
+    try{
+      await logout()
+      navigate('/login')
+    } catch {
+      setError('Failed to log out')
+    }
+  }
+
+  return (
+    <>
+      <Card>
+        <Card.Body>
+          <h2 className="text-center mb-4">Dashboard</h2>
+          {error && <Alert variant="danger">{error}</Alert>}
+          <strong>Email:</strong>
+        </Card.Body>
+      </Card>
+      <div className="w-100 text-center mt-2">
+        <Button variant="link" onClick={handleLogout}>
+          Log Out
+        </Button>
+      </div>
+    </>
+  )
+};

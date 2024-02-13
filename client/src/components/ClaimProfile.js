5a2f83ff5a700b0ae77a26dc87264bed159039c0
SashaGordin <55962431+SashaGordin@users.noreply.github.com>
2024-02-12 23:58:48 -0800

    added phone number verification with Twilio, and step 3 which allows user to verify their information

--- client/src/components/ClaimProfile.js
+++ client/src/components/ClaimProfile.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
 import Step1 from './ClaimProfile/Step1';
 import Step2 from './ClaimProfile/Step2';
+import Step3 from './ClaimProfile/Step3';
 
 export default function ClaimProfile() {
   const [currentStep, setCurrentStep] = useState(1);
+  const [providerInfo, setProviderInfo] = useState(null);
 
   const handleNext = () => {
     setCurrentStep(currentStep + 1);
@@ -15,8 +17,9 @@ export default function ClaimProfile() {
 
   return(
     <>
-      {currentStep === 1 && <Step1 onNext={handleNext} />}
-      {currentStep === 2 && <Step2 onNext={handleNext} onBack={handleBack} />}
+      {currentStep === 1 && <Step1 onNext={handleNext} setProviderInfo={setProviderInfo} />}
+      {currentStep === 2 && <Step2 onNext={handleNext} onBack={handleBack} providerInfo={providerInfo}/>}
+      {currentStep === 3 && <Step3 onNext={handleNext} onBack={handleBack} providerInfo={providerInfo}/>}
     </>
   )
 };

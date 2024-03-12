a92bd27a04bc94d06c74688cfa5195e6a3342f57
SashaGordin <55962431+SashaGordin@users.noreply.github.com>
2024-03-12 12:04:20 -0700

    merge top nav

--- client/src/components/TopNav.js
+++ client/src/components/TopNav.js
@@ -1,33 +1,31 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Nav } from "react-bootstrap";
 import { NavDropdown } from "react-bootstrap";
-import { getDoc, doc } from 'firebase/firestore';
-import { firestore } from '../firebase'; // Import your Firestore instance
-import { useAuth } from "../contexts/AuthContext"
+import { getDoc, doc } from "firebase/firestore";
+import { firestore } from "../firebase"; // Import your Firestore instance
+import { useAuth } from "../contexts/AuthContext";
 
+export default function TopNav() {
+	const [role, setRole] = useState("");
+	const { currentUser } = useAuth();
 
-const TopNav = () => {
-    const [ role, setRole ] = useState('');
-    const { login, currentUser } = useAuth()
-
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const userDocRef = doc(firestore, "users", currentUser.uid);
-                const userDocSnapshot = await getDoc(userDocRef);
-                if (userDocSnapshot.exists()) {
-                    const userData = userDocSnapshot.data();
-                    setRole(userData.role);
-                }
-            } catch (error) {
-                console.log("the error: ", error);
-            }
-        };
-        if (currentUser) {
-            fetchData(); // Call the async function immediately
-        }
-    }, []);
-
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const userDocRef = doc(firestore, "users", currentUser.uid);
+				const userDocSnapshot = await getDoc(userDocRef);
+				if (userDocSnapshot.exists()) {
+					const userData = userDocSnapshot.data();
+					setRole(userData.role);
+				}
+			} catch (error) {
+				console.log("the error: ", error);
+			}
+		};
+		if (currentUser) {
+			fetchData(); // Call the async function immediately
+		}
+	}, []);
 
 	return (
 		<>
@@ -39,81 +37,81 @@ const TopNav = () => {
 					</div>
 
 					<div className="topNavRight">
-                        {role === 'client' && (
-                            <Nav variant="pills">
-                                <Nav.Item>
-                                    <Nav.Link href="/" title="Item">
-                                        Browse
-                                    </Nav.Link>
-                                </Nav.Item>
+						{role === "client" && (
+							<Nav variant="pills">
+								<Nav.Item>
+									<Nav.Link href="/" title="Item">
+										Browse
+									</Nav.Link>
+								</Nav.Item>
 
-                                <Nav.Item>
-                                    <Nav.Link href="##" title="Item">
-                                        Hub
-                                    </Nav.Link>
-                                </Nav.Item>
+								<Nav.Item>
+									<Nav.Link href="##" title="Item">
+										Hub
+									</Nav.Link>
+								</Nav.Item>
 
-                                <Nav.Item>
-                                    <Nav.Link href="###" title="Item">
-                                        Inbox
-                                    </Nav.Link>
-                                </Nav.Item>
+								<Nav.Item>
+									<Nav.Link href="###" title="Item">
+										Inbox
+									</Nav.Link>
+								</Nav.Item>
 
-                                <Nav.Item>
-                                    <Nav.Link href="/client-menu" title="Item">
-                                        Menu
-                                    </Nav.Link>
-                                </Nav.Item>
+								<Nav.Item>
+									<Nav.Link href="/client-menu" title="Item">
+										Menu
+									</Nav.Link>
+								</Nav.Item>
 
-                                <NavDropdown title="Dropdown" id="nav-dropdown">
+								{/* <NavDropdown title="Dropdown" id="nav-dropdown">
                                     <NavDropdown.Item>Action</NavDropdown.Item>
                                     <NavDropdown.Item>Another action</NavDropdown.Item>
                                     <NavDropdown.Item>Something else here</NavDropdown.Item>
                                     <NavDropdown.Divider />
                                     <NavDropdown.Item>Separated link</NavDropdown.Item>
-                                </NavDropdown>
-                            </Nav>
-                        )}
-                        {role === 'provider' && (
-                            <Nav variant="pills">
-                                <Nav.Item>
-                                    <Nav.Link href="/care-provider-dashboard" title="Item">
-                                        Home
-                                    </Nav.Link>
-                                </Nav.Item>
+                                </NavDropdown> */}
+							</Nav>
+						)}
+						{role === "provider" && (
+							<Nav variant="pills">
+								<Nav.Item>
+									<Nav.Link href="/care-provider-dashboard" title="Item">
+										Home
+									</Nav.Link>
+								</Nav.Item>
 
-                        <Nav.Item>
-                            <Nav.Link href="/" title="Item">HOME</Nav.Link>
-                        </Nav.Item>
+								<Nav.Item>
+									<Nav.Link href="##" title="Item">
+										My Listings
+									</Nav.Link>
+								</Nav.Item>
 
-                        <Nav.Item>
-                            <Nav.Link href="##" title="Item">LINK2</Nav.Link>
-                        </Nav.Item>
+								<Nav.Item>
+									<Nav.Link href="##" title="Item">
+										Inbox
+									</Nav.Link>
+								</Nav.Item>
 
-                        <Nav.Item>
-                            <Nav.Link href="###" title="Item">LINK3</Nav.Link>
-                        </Nav.Item>
+								<Nav.Item>
+									<Nav.Link href="/provider-menu" title="Item">
+										Menu
+									</Nav.Link>
+								</Nav.Item>
 
-                        <NavDropdown title="Dropdown" id="nav-dropdown">
+								{/* <NavDropdown title="Dropdown" id="nav-dropdown">
                             <NavDropdown.Item>Action</NavDropdown.Item>
                             <NavDropdown.Item>Another action</NavDropdown.Item>
                             <NavDropdown.Item>Something else here</NavDropdown.Item>
                             <NavDropdown.Divider />
                             <NavDropdown.Item>Separated link</NavDropdown.Item>
-                        </NavDropdown>
+                        </NavDropdown> */}
+							</Nav>
+						)}
+					</div>
 
-                    </Nav>
-
-                </div>
-
-                <div className="clear"></div>
-
-            </div>
-
-        </div>
-
-    </>
-
-);
-
-export default TopNav;
+					<div className="clear"></div>
+				</div>
+			</div>
+		</>
+	);
+}

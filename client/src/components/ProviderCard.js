7622d0c01c5dd9869a3895dc8e06cb7876dac605
SashaGordin <55962431+SashaGordin@users.noreply.github.com>
2024-05-08 12:35:09 -0700

    added view profile

--- client/src/components/ProviderCard.js
+++ client/src/components/ProviderCard.js
@@ -1,31 +1,57 @@
-import React from 'react';
-import { Card } from 'react-bootstrap';
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+import ProviderProfileCard from "./ProviderProfileCard";
 
 const ProviderCard = ({ provider, onClick }) => {
-  const {
-    FacilityName,
-    LocationAddress,
-    LocationCity,
-    LocationState,
-    LocationZipCode,
-    TelephoneNmbr,
-    // Add more fields as needed
-  } = provider;
+	const {
+		FacilityName,
+		LocationAddress,
+		LocationCity,
+		LocationState,
+		LocationZipCode,
+		TelephoneNmbr,
+		profilePicPath,
+		FacilityPOC,
+		// Add more fields as needed
+	} = provider;
 
-  return (
-    <Card>
-      <Card.Body>
-        <div onClick={() => onClick(provider)}>
-          <Card.Title>{FacilityName}</Card.Title>
-        </div>
-        <Card.Text>
-          <strong>Address:</strong> {LocationAddress}, {LocationCity}, {LocationState} {LocationZipCode}<br />
-          <strong>Telephone:</strong> {TelephoneNmbr}
-          {/* Add more fields here */}
-        </Card.Text>
-      </Card.Body>
-    </Card>
-  );
+  console.log(provider)
+
+	return (
+		<Card>
+			<Card.Body
+				style={{
+					display: "flex",
+					flexDirection: "row",
+					fontSize: "12px",
+					maxHeight: "200px",
+				}}
+				onClick={() => onClick(provider)}
+			>
+				<ProviderProfileCard provider={provider} />
+				<div
+					style={{
+						display: "flex",
+						flexDirection: "column",
+						flex: 1,
+						margin: "5px",
+					}}
+				>
+					<img
+						style={{
+							maxWidth: "100%",
+							width: "auto",
+							height: "auto",
+							maxHeight: "calc(100% - 20px)", // Adjust the height relative to the parent container's height
+						}} // Set max-width and max-height for the image
+						src={profilePicPath}
+						alt="Profile pic"
+					/>
+					<div style={{ textAlign: "center" }}>{FacilityName}</div>
+				</div>
+			</Card.Body>
+		</Card>
+	);
 };
 
 export default ProviderCard;

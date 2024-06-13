82cd7349fc8cd9b30fd7b86e6492324643510b50
Jim Dee <jim@generativenfts.io>
2024-06-12 20:37:11 -0500

    misc CSS updates and new home page - to be refined still

--- client/src/components/ProviderCard.js
+++ client/src/components/ProviderCard.js
@@ -21,40 +21,45 @@ const ProviderCard = ({ provider, onClick }) => {
 
 	return (
 		<>
-			<Card>
-				<Card.Body
-					style={{
-						display: "flex",
-						flexDirection: "row",
-						fontSize: "12px",
-						maxHeight: "200px",
-					}}
-					onClick={() => onClick(provider)}
-				>
-					<ProviderProfileCard provider={provider} />
-					<div
+			<div className="providerCard">
+				<Card>
+					<Card.Body
 						style={{
 							display: "flex",
-							flexDirection: "column",
-							flex: 1,
-							margin: "5px",
+							flexDirection: "row",
+							fontSize: "12px",
+							maxHeight: "200px",
 						}}
+						onClick={() => onClick(provider)}
 					>
-						<img
+						<ProviderProfileCard provider={provider} />
+						<div
 							style={{
-								maxWidth: "100%",
-								width: "auto",
-								height: "auto",
-								maxHeight: "calc(100% - 20px)", // Adjust the height relative to the parent container's height
-							}} // Set max-width and max-height for the image
-							src={homePhotos[0]}
-							alt="Profile pic"
-							onClick={handleImageClick}
-						/>
-						<div style={{ textAlign: "center" }}>{FacilityName}</div>
-					</div>
-				</Card.Body>
-			</Card>
+								display: "flex",
+								flexDirection: "column",
+								flex: 1,
+								margin: "5px",
+							}}
+						>
+							<img
+								style={{
+									maxWidth: "100%",
+									width: "auto",
+									height: "auto",
+									maxHeight: "calc(100% - 20px)", 
+									marginBottom:3,
+									borderRadius:10
+									// Adjust the height relative to the parent container's height
+								}} // Set max-width and max-height for the image
+								src={homePhotos[0]}
+								alt="Profile pic"
+								onClick={handleImageClick}
+							/>
+							<div style={{ textAlign: "center" }}>{FacilityName}</div>
+						</div>
+					</Card.Body>
+				</Card>
+			</div>
 			{showModal && (
 				<PropertyPhotoModal showModal={showModal} setShowModal={setShowModal} FacilityName={FacilityName} homePhotos={homePhotos} roomPhotos={roomPhotos} />
 			)}
